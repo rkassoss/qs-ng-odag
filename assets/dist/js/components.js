6cfe07d1205877d3ae61dd5797f30a225fae6736
Austin gerner <agerner1146@gmail.com>
2018-09-05 08:48:15 -0400

    'ranUpdates'

--- assets/dist/js/components.js
+++ assets/dist/js/components.js
@@ -76,6 +76,55 @@
     });
 
 } ());
+define( 'topHeader',function () {
+    
+    function topHeader() {
+        topHeaderController.$inject = ['dataService','qlikService'];
+        function topHeaderController(dataService,qlikService) {
+            var vm = this;
+
+            vm.toggleSidebar = toggleSidebar;
+            vm.toggleNav = toggleNav;
+
+            vm.sidebarIn = false;
+            vm.navigation = false;
+
+            function toggleNav() {
+                vm.navigation = !vm.navigation;
+            }
+
+            function toggleSidebar() {
+                vm.sidebarIn = !vm.sidebarIn;
+            }
+
+            function dataLastFrom() {
+                qlikService.getApp().getAppLayout(function(layout){
+                    // console.log(layout);
+                    vm.relaodTime = layout.qLastReloadTime;
+                });
+            }
+
+            function getFilters() {
+                qlikService.getApp().getObject('nativeFilters','ycppXj');
+            }
+
+            init();
+            function init() {
+                dataLastFrom();
+                getFilters();
+                qlikService.getApp().getObject('CurrentSelections','CurrentSelections');
+            }
+        }
+        return {
+            bindings: {},
+            controller: topHeaderController,
+            controllerAs: 'th',
+            templateUrl: 'app/components/topHeader/topHeader.component.html'
+        }
+    }
+
+    return topHeader();
+});
 (function () {
     'use strict';
 
@@ -157,55 +206,6 @@
             return senseObject();
         });
 } ());
-define( 'topHeader',function () {
-    
-    function topHeader() {
-        topHeaderController.$inject = ['dataService','qlikService'];
-        function topHeaderController(dataService,qlikService) {
-            var vm = this;
-
-            vm.toggleSidebar = toggleSidebar;
-            vm.toggleNav = toggleNav;
-
-            vm.sidebarIn = false;
-            vm.navigation = false;
-
-            function toggleNav() {
-                vm.navigation = !vm.navigation;
-            }
-
-            function toggleSidebar() {
-                vm.sidebarIn = !vm.sidebarIn;
-            }
-
-            function dataLastFrom() {
-                qlikService.getApp().getAppLayout(function(layout){
-                    // console.log(layout);
-                    vm.relaodTime = layout.qLastReloadTime;
-                });
-            }
-
-            function getFilters() {
-                qlikService.getApp().getObject('nativeFilters','ycppXj');
-            }
-
-            init();
-            function init() {
-                dataLastFrom();
-                getFilters();
-                qlikService.getApp().getObject('CurrentSelections','CurrentSelections');
-            }
-        }
-        return {
-            bindings: {},
-            controller: topHeaderController,
-            controllerAs: 'th',
-            templateUrl: 'app/components/topHeader/topHeader.component.html'
-        }
-    }
-
-    return topHeader();
-});
 (function () {
     'use strict';
 
